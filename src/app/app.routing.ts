64b5a3ebac24a4f325f65522dc36172c69f66bfa
Eric <ericsarpong@hotmail.com>
2018-08-08 00:46:55 -0400

    added create account route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/components/login.component';
 import {HomeComponent} from './home/home.component';
+import { CreateAccountComponent } from './create-account/components/create-account.component';
 
 export const routing = RouterModule.forRoot([
     {path: 'login', component: LoginComponent},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'create', component: CreateAccountComponent}
  ]);
