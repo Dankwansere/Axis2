e29827300de3a89811dfb31a47ae4c26b5167507
Eric <ericsarpong@hotmail.com>
2018-08-08 19:03:30 -0400

    importing shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {routing} from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
+
 // feature modules
 import {LoginModule} from './login/login.module';
 import {CreateAccountModule} from './create-account/create-account.module';
+import {SharedModule} from './shared/shared.module';
 
 // Components
 import { AppComponent } from './app.component';
@@ -13,14 +15,15 @@ import { HomeComponent } from './home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
   imports: [
-    BrowserModule,  BrowserAnimationsModule, MatDialogModule,
-     LoginModule, CreateAccountModule, routing, HttpClientModule
+    BrowserModule, BrowserAnimationsModule, MatDialogModule,
+     LoginModule, SharedModule, CreateAccountModule, routing, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
