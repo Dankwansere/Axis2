96240f0a0114fb7f1cb166dc686edb0570982663
Eric <ericsarpong@hotmail.com>
2018-08-08 19:03:07 -0400

    extending base component, and adding logic for spinner

--- src/app/login/components/login.component.ts
+++ src/app/login/components/login.component.ts
@@ -5,20 +5,23 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import {ErrorMessage} from '../../commons/error-message';
+import { BaseComponent } from '../../shared/base/component/base.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: '../view/login.component.html',
   styleUrls: ['../view/login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent extends BaseComponent implements OnInit {
 
 
   private loginForm: FormGroup;
   private errorMessage;
 
   constructor(private fb: FormBuilder, private router: Router,
-     private dialogRef: MatDialogRef<LoginComponent>, private loginService: LoginService) { }
+     private dialogRef: MatDialogRef<LoginComponent>, private loginService: LoginService) { 
+       super();
+     }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
@@ -28,19 +31,22 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-
+    this.isLoading = true;
     if (this.loginForm.valid) {
       this.loginService.login(this.loginForm.value).subscribe((resp: any) => {
         console.log('Response: ', resp);
         if (resp.status === 'Valid') {
+          this.isLoading = false;
           this.dialogRef.close();
           this.router.navigate(['home']);
         } else if (resp.status === 'Invalid') {
+          this.isLoading = false;
           this.errorMessage = ErrorMessage.INVALID_LOGIN;
         }
       });
 
     } else if (this.loginForm.invalid) {
+      this.isLoading = false;
       console.log('invalid form');
       this.errorMessage = 'All fields required!';
     }
