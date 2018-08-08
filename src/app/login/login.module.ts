3070f83a6a3723a67b94aaa36435952a758976c7
Eric <ericsarpong@hotmail.com>
2018-08-08 19:02:32 -0400

    importing shared module and commonmodule

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {LoginComponent} from './components/login.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import {LoginService} from './services/login.service';
 import {BaseService} from '../shared/services/base.service';
-
+import {SharedModule} from '../shared/shared.module';
 @NgModule({
     declarations: [LoginComponent],
-    imports: [ReactiveFormsModule, MatInputModule,  MatButtonModule],
+    imports: [ReactiveFormsModule, CommonModule, SharedModule, MatInputModule,  MatButtonModule],
     exports: [],
     providers: [LoginService, BaseService]
 })
