7f82398aea94512ce85b2f8a8ce13b7380f0c190
Eric <ericsarpong@hotmail.com>
2018-08-08 21:41:28 -0400

    implementing stepper to navigate between 3 forms

--- src/app/create-account/components/create-account.component.ts
+++ src/app/create-account/components/create-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-create-account',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
+  private provinces;
   constructor() { }
 
   ngOnInit() {
+    this.loadListOfProvinces();
+  }
+
+  private goBack(stepper: MatStepper) {
+    stepper.previous();
+  }
+
+  private loadListOfProvinces() {
+
+    this.provinces = [
+      {value: 'AB', viewValue: 'Alberta'},
+      {value: 'BC', viewValue: 'British Columbia'},
+      {value: 'MB', viewValue: 'Manitoba'},
+      {value: 'NB', viewValue: 'New Brunswick'},
+      {value: 'NL', viewValue: 'Newfoundland and Labrador'},
+      {value: 'NT', viewValue: 'North west territories'},
+      {value: 'NU', viewValue: 'Nunavut'},
+      {value: 'ON', viewValue: 'Ontario'},
+      {value: 'PE', viewValue: 'Prince Edward Island'},
+      {value: 'QC', viewValue: 'Quebec'},
+      {value: 'SK', viewValue: 'Saskatchewan'},
+      {value: 'YK', viewValue: 'Yukon'}
+    ];
+
   }
 
 }
--- src/app/create-account/create-account.module.ts
+++ src/app/create-account/create-account.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { CreateAccountComponent } from './components/create-account.component';
 import {MatStepperModule} from '@angular/material/stepper';
-
+import {SharedModule} from '../shared/shared.module';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
     declarations: [CreateAccountComponent],
-    imports: [MatStepperModule],
+    imports: [CommonModule, MatButtonModule, MatStepperModule, MatSelectModule, MatInputModule, SharedModule],
     exports: [],
     providers: []
 
