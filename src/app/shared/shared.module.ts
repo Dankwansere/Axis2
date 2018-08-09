d0de7817707031509eb9e61a3567bff4e5e21f67
Eric <ericsarpong@hotmail.com>
2018-08-08 21:41:46 -0400

    importing new modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 import { SpinnerComponent } from './spinner/components/spinner.component';
 import {PasswordComponent} from './password/components/password.component';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatInputModule} from '@angular/material/input';
 import { BaseComponent } from './base/component/base.component';
 
 @NgModule ({
     declarations: [SpinnerComponent, PasswordComponent, BaseComponent],
-    imports: [MatProgressSpinnerModule],
+    imports: [CommonModule, MatInputModule, ReactiveFormsModule, MatProgressSpinnerModule],
     exports: [SpinnerComponent, PasswordComponent],
     providers: []
 })
