fc43cbab288385ea5efbceccb07d4d4836417926
Eric <ericsarpong@hotmail.com>
2018-08-09 20:19:49 -0400

    imported ReactiveFormsModule and added CommonServices to providers

--- src/app/create-account/create-account.module.ts
+++ src/app/create-account/create-account.module.ts
@@ -6,11 +6,13 @@ import {SharedModule} from '../shared/shared.module';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import {CommonServices} from '../shared/services/common.service';
 @NgModule({
     declarations: [CreateAccountComponent],
-    imports: [CommonModule, MatButtonModule, MatStepperModule, MatSelectModule, MatInputModule, SharedModule],
+    imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatStepperModule, MatSelectModule, MatInputModule, SharedModule],
     exports: [],
-    providers: []
+    providers: [CommonServices]
 
 })
 
