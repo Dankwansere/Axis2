7a6f54d1ed7456a0c5d7823b34eec829e507ef03
Eric <ericsarpong@hotmail.com>
2018-08-11 15:09:00 -0400

    ErrorMessage update

--- src/app/commons/error-message.ts
+++ src/app/commons/error-message.ts
@@ -1,4 +1,5 @@
 export enum ErrorMessage {
     INVALID_LOGIN = 'Invalid credentials entered, please try again',
-    USERNAME_TAKEN = 'Sorry, that username is already taken'
+    USERNAME_TAKEN = 'Sorry, that username is already taken',
+    EMAIL_TAKEN = 'Sorry, that email is already taken'
 }
