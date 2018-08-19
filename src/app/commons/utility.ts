5cdaa6dff71a2d7d5a7baccc5a452e69e99411ed
Eric <ericsarpong@hotmail.com>
2018-08-19 17:36:11 -0400

    utility class to handle common functions

---
+++ src/app/commons/utility.ts
@@ -0,0 +1,17 @@
+export class Utility {
+
+    /**
+     * Compares two strings
+     * @param str1 string 1 to compare
+     * @param str2 string 2 to compare
+     */
+    static compareTwoStrings(str1: string, str2: string): boolean {
+
+        if (str1 === str2) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
