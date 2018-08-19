788534c0af2a86bffad75acbb5d4678cd7648045
Eric <ericsarpong@hotmail.com>
2018-08-19 13:29:06 -0400

    fixed imports

--- src/app/shared/services/common.service.ts
+++ src/app/shared/services/common.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
-import { environment } from 'environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class CommonServices {
