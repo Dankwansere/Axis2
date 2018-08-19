abe89a1b80549fdc68ea8f85117ca43e7065f002
Eric <ericsarpong@hotmail.com>
2018-08-19 17:35:48 -0400

    new error messages added

--- src/app/commons/error-message.ts
+++ src/app/commons/error-message.ts
@@ -1,5 +1,7 @@
 export enum ErrorMessage {
     INVALID_LOGIN = 'Invalid credentials entered, please try again',
     USERNAME_TAKEN = 'Sorry, that username is already taken',
-    EMAIL_TAKEN = 'Sorry, that email is already taken'
+    EMAIL_TAKEN = 'Sorry, that email is already taken',
+    PASSWORD_DO_NOT_MATCH = 'The passwords do not match, please try again',
+    UNEXPECTED_ERROR = 'An unexpected error has occured, please try again'
 }
