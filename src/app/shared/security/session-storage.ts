56f9250f4857464c85a915cc41db49166a5ba7af
Eric <ericsarpong@hotmail.com>
2018-08-19 13:32:02 -0400

    Session Storage class wrapper

---
+++ src/app/shared/security/session-storage.ts
@@ -0,0 +1,28 @@
+
+export class SessionStorage {
+
+    /**
+     * @param key A DOMString containing the name of the key you want to create/update.
+     * @param data A DOMString containing the value you want to give the key you are creating/updating.
+     */
+    static setDataInSession(key: string, data: string) {
+         sessionStorage.setItem(key, data);
+    }
+
+    /**
+     * @param key A DOMString containing the name of the key you want to remove
+     */
+    static removeDataFromSession(key: string) {
+        sessionStorage.removeItem(key);
+    }
+
+    /**
+     * 
+     * @param key A DOMString contaning the name of the key you want to be returned
+     */
+    static returnDataFromSession(key: string) {
+        return sessionStorage.getItem(key);
+    }
+
+
+}
