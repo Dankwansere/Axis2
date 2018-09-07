c795af8e861c804d18e20ee5eb9f651ffd5a0bf2
Eric <ericsarpong@hotmail.com>
2018-09-06 20:45:52 -0400

    route update

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,11 @@
-import {Routes, RouterModule} from '@angular/router';
+import {Route} from '@angular/router';
 import {LoginComponent} from './login/components/login.component';
 import {HomeComponent} from './home/home.component';
 import { CreateAccountComponent } from './create-account/components/create-account.component';
 
-export const routing = RouterModule.forRoot([
+export const routing: Route[] = [
     {path: 'login', component: LoginComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'create', component: CreateAccountComponent}
- ]);
+    {path: 'create', component: CreateAccountComponent},
+ ];
+
