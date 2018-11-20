b6d934dbc9f6f03d08a5fd03bd66e8edfcfb7541
Eric <ericsarpong@hotmail.com>
2018-11-20 04:40:15 -0500

    new error messages

--- src/app/commons/error-message.ts
+++ src/app/commons/error-message.ts
@@ -3,5 +3,6 @@ export enum ErrorMessage {
     USERNAME_TAKEN = 'Sorry, that username is already taken',
     EMAIL_TAKEN = 'Sorry, that email is already taken',
     PASSWORD_DO_NOT_MATCH = 'The passwords do not match, please try again',
-    UNEXPECTED_ERROR = 'An unexpected error has occured, please try again'
+    UNEXPECTED_ERROR = 'An unexpected error has occured, please try again',
+    ALL_FIELDS_REQUIRED = 'All fields required!'
 }
