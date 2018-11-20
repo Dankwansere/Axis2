6da29386d2491302578486b52f467658bc4f8a0c
Eric <ericsarpong@hotmail.com>
2018-11-20 04:40:02 -0500

    new enums

--- src/app/commons/constants.ts
+++ src/app/commons/constants.ts
@@ -1,3 +1,17 @@
 export enum Constants {
-    USER_SESSION_KEY = 'AUTH_USER'
+    USER_SESSION_KEY = 'AUTH_USER', VALID = 'Valid', INVALID = 'invalid',
+    PASSWORD = 'password', USERNAME = 'username', EMAIL = 'EMAIL', EMPTY = ''
+}
+
+export enum NumberConst {
+    ZER0 = 0, ONE = 1, TWO = 2, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7,
+    EIGHT = 8, NINE = 9, TEN = 10
+}
+
+export enum RoutingConstants {
+    HOME = 'home'
+}
+
+export enum SessionConstants {
+    AUTH = 'authorization'
 }
